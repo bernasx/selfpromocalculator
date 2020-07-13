22c37348a3ca6e0fd6f45672a61c03711bff07b1
bernasx <38398866+bernasx@users.noreply.github.com>
2020-07-13 18:06:46 +0100

    I know math

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ document.getElementById("calculateBtn").addEventListener("click", function(){
     let ownPostComments = parseInt(document.getElementById("ownPostComments").value);
     let selfPromoItems = parseInt(document.getElementById("selfPromoItems").value);
     
-    let result = submissions + totalComments;
-    result = result - ownPostComments - selfPromoItems;
+    let result = submissions + (totalComments - ownPostComments);
     result = selfPromoItems / result;
 
     result = Math.round((result + Number.EPSILON) * 100);
     document.getElementById("result").innerHTML = "Result: " + result + "%";
+    if(result > 10) {
+      document.getElementById("result").style.color = "red";
+    } else {
+      document.getElementById("result").style.color = "green";
+    }
+    
   });
 
   document.getElementById("clearBtn").addEventListener("click", function(){
