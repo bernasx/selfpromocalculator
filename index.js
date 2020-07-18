cbcd23a9faef924abbd07fe5a393eb162a6c018a
bernasx <38398866+bernasx@users.noreply.github.com>
2020-07-18 23:22:33 +0100

    added itemsLeft to reach ratio

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ document.getElementById("calculateBtn").addEventListener("click", function(){
     let totalComments = parseInt(document.getElementById("totalComments").value);
     let ownPostComments = parseInt(document.getElementById("ownPostComments").value);
     let selfPromoItems = parseInt(document.getElementById("selfPromoItems").value);
-    
+
+    //calculate result
     let result = submissions + (totalComments - ownPostComments);
     result = selfPromoItems / result;
+    //round the result
+    result*= 100;
+    result = result.toFixed(2);
+    //calculate how many items till within ratio
+    let itemsLeft = (selfPromoItems * 10) - (submissions + totalComments - ownPostComments);
 
-    result = Math.round((result + Number.EPSILON) * 100);
+    //print out result
     document.getElementById("result").innerHTML = "Result: " + result + "%";
     if(result > 10) {
       document.getElementById("result").style.color = "red";
     } else {
       document.getElementById("result").style.color = "green";
+    }
+
+    //print out items left
+    if(itemsLeft > 0) {
+      document.getElementById("itemsLeft").innerHTML = "Posts/Comments left to be within the ratio: " + itemsLeft;
+    } else {
+      document.getElementById("itemsLeft").innerHTML = "Posts/Comments left to be within the ratio: " + '0';
     }
     
   });
